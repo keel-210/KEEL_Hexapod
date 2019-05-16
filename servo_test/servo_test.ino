ad1419406955fc2373e028519f46c69f01c441a2
keel-210 <crafters_touhou@yahoo.co.jp>
2019-05-16 14:29:16 +0900

    LegBlueprint&ServoTest

--- servo_test/servo_test.ino
+++ servo_test/servo_test.ino
@@ -2,21 +2,29 @@
 
 #include <Servo.h>
 
-Servo myservo;
+Servo myservo, myservo1, myservo2;
 
 int pos = 0;
 
-void setup() {
-  myservo.attach( 9 );
+void setup()
+{
+  myservo.attach(9);
+  myservo1.attach(10);
+  myservo2.attach(11);
 }
 
-void loop() {
-  for ( pos = 0; pos < 180; pos += 1 ) {
-    myservo.write( pos );
-    delay( 15 );
+void loop()
+{
+  for (pos = 0; pos < 15; pos += 1)
+  {
+    myservo.write(pos);
+    delay(100);
   }
-  for ( pos = 180; pos > 0; pos -= 1 ) {
-    myservo.write( pos );
-    delay( 15 );
+  for (pos = 15; pos > 0; pos -= 1)
+  {
+    myservo.write(pos);
+    delay(100);
   }
+  myservo1.write(90);
+  myservo2.write(180);
 }
